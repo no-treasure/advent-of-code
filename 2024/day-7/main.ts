1881aaac4573079f5346b93b466447444449735d
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2024-12-11 15:14:38 +0200

    Day 7 part 2

--- 2024/day-7/main.ts
+++ 2024/day-7/main.ts
@@ -1,9 +1,11 @@
 import { partOne } from './part-one.ts'
+import { partTwo } from './part-two.ts'
 
 const input = await Deno.readTextFile('./day-7/input.txt')
 
 const formattedInput = input.split('\n').map((line) => line.split(':').map((part) => part.trim()))
 
 console.log({
-  partOne: partOne(formattedInput)
+  partOne: partOne(formattedInput),
+  partTwo: partTwo(formattedInput)
 })
